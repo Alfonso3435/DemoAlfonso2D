930deca708101037a857b5bcb8cc103488f9a7e6
Alfonso3435 <141585287+Alfonso3435@users.noreply.github.com>
2025-03-29 18:25:58 -0600

    Redes
    Funcionamiento en el script de redes para cumplir con la tarea "Redes en Unity"

--- Assets/Scripts/Red.cs
+++ Assets/Scripts/Red.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections;
 using UnityEngine;
@@ -6,32 +5,36 @@ using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
-
 public class Red : MonoBehaviour
 {
-
     private TextField tfNombreUsuario;
     private TextField tfContraseña;
-
     private Label textError;
+    private string usuarioActual;
+
+    [Serializable]
     public struct DatosUsuario
     {
         public string nombreUsuario;
         public string contraseña;
+    }
 
+    [Serializable]
+    public struct RespuestaServidor
+    {
+        public bool success;
+        public string mensaje;
+        public string horaInicio;
     }
 
     void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         tfNombreUsuario = root.Q<TextField>("Usuario");
-        
         tfContraseña = root.Q<TextField>("Contrasena");
-
         textError = root.Q<Label>("Error");
 
         Button botonEnviar = root.Q<Button>("Enviar");
-
         botonEnviar.clicked += EnviarDatosJSON;
     }
 
@@ -42,27 +45,73 @@ public class Red : MonoBehaviour
 
     private IEnumerator SubirDatosJSON()
     {
-        DatosUsuario datos;
-        datos.nombreUsuario = tfNombreUsuario.value;
-        datos.contraseña = tfContraseña.value;
+        DatosUsuario datos = new DatosUsuario
+        {
+            nombreUsuario = tfNombreUsuario.value,
+            contraseña = tfContraseña.value
+        };
 
         string datosJSON = JsonUtility.ToJson(datos);
-        print(datosJSON);
+        UnityWebRequest request = new UnityWebRequest("http://192.168.0.104:3000/unity/recibeJSON", "POST");
+        
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(datosJSON);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
 
-        UnityWebRequest request = UnityWebRequest.Post("http://10.48.87.168:3000", datosJSON, "application/json");
         yield return request.SendWebRequest();
 
-        //DESPUES DE QUE SE DESCARGA EL ARCHIVO Y PASA UN TIEMPO
         if (request.result == UnityWebRequest.Result.Success)
         {
-            print("Correcto");
-            SceneManager.LoadScene("Menu");
+            RespuestaServidor respuesta = JsonUtility.FromJson<RespuestaServidor>(request.downloadHandler.text);
+
+            if (respuesta.success)
+            {
+                usuarioActual = datos.nombreUsuario;
+                SceneManager.LoadScene("EscenaMenu");
+            }
+            else
+            {
+                textError.text = respuesta.mensaje;
+            }
         }
         else
         {
-            textError.text = "Error de conexión: "+request.responseCode;
-            print("Error" + request.responseCode);
+            textError.text = "Error de conexión: " + request.responseCode;
         }
     }
 
+    private IEnumerator FinalizarSesion()
+{
+    DatosUsuario datos = new DatosUsuario
+    {
+        nombreUsuario = usuarioActual
+    };
+
+    string datosJSON = JsonUtility.ToJson(datos);
+    UnityWebRequest request = new UnityWebRequest("http://192.168.0.104:3000/unity/finalizarSesion", "POST");
+
+    byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(datosJSON);
+    request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+    request.downloadHandler = new DownloadHandlerBuffer();
+    request.SetRequestHeader("Content-Type", "application/json");
+
+    yield return request.SendWebRequest();
+
+    if (request.result == UnityWebRequest.Result.Success)
+    {
+        Debug.Log("Sesión finalizada correctamente");
+    }
+    else
+    {
+        Debug.LogError("Error al finalizar sesión: " + request.responseCode);
+    }
 }
+
+void OnApplicationQuit()
+{
+    StartCoroutine(FinalizarSesion());
+}
+
+}
+ 
